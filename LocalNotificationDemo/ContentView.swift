2695aa98dc1cda8e20eb6f831616b5e1705527b8
itsuki <itsuki.enjyoy@gmail.com>
2024-07-13 08:15:05 +0900

    Initial Commit

---
+++ LocalNotificationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalNotificationDemo
+//
+//  Created by Itsuki on 2024/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalNotificationDemo/LocalNotificationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationDemoApp.swift
+//  LocalNotificationDemo
+//
+//  Created by Itsuki on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
